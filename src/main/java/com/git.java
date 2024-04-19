27ef9b2f2ebbc477c81fa6a168e39ed571b1afa0
yffs <yffs@qq.com>
2024-04-19 17:24:33 +0800

    hotfix1 ci

--- src/main/java/com/git.java
+++ src/main/java/com/git.java
@@ -3,6 +3,8 @@ package com;
 public class git {
     public static void main(String[] args) {
         System.out.println("h1");
+        System.out.println("he");
+        System.out.println("h2");
     }
 
 }
